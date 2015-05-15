2faed073bafe231f7bb72231b0af1d862f945924
MCPEGamerJPatGitHub <MCPEGamerJPatGitHub@users.noreply.github.com>
2015-05-15 09:38:38 +0400

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,9 @@
 <php
 
+use pocketmine\player\playerloginevent;
+use LexyPvP\Website\login;
 
 namespace LexyPvP; ///LexyPvP owns it.
+
+>
+  /// This is a developing plugin which only allows players to register on LexyPvP's official website. Once registered, you need to type your password in the chat directly. 
